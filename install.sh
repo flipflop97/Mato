88ee22002c44102f0dfcc9a000e182512ce4b2cc
Philip Goto <philip.goto@gmail.com>
2016-08-15 19:38:37 +0200

    Gnome Software

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 rm -rf ~/.icons/Mato/
 cp -r ./ ~/.icons/Mato/
+gsettings reset org.gnome.desktop.interface icon-theme
+gsettings set org.gnome.desktop.interface icon-theme "Mato"
